52fdb0931f0262aa2336effdbf33d3f3915f6d55
Hamidreza Heidarzadeh <heidarzadeh.hamidreza@gmail.com>
2018-01-07 16:53:40 -0500

    Project Files Added

---
+++ RollTheBall/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject Player;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + offset;
+    }
+}
---
+++ RollTheBall/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float movementSpeed = 10.0f;
+    public  UnityEngine.UI.Text t;
+    public UnityEngine.UI.Text w;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        t.text = "Objects Collected: "+ count.ToString();
+        w.gameObject.SetActive(false);
+        //Debug.Log(count);
+    }
+
+    void Update()
+    {
+        if (count==8)
+        {
+            w.gameObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * movementSpeed);
+    }
+     
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            t.text = "Objects Collected: " + count.ToString();
+            //Debug.Log(count);
+        }
+    }
+
+
+}
